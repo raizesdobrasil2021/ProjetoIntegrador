88003f3b54b068dca04a94aa8a898de973d88516
Igor Morais <igorhenrique@outlook.com>
2021-07-29 10:53:29 -0300

    criação de model e configuração de banco

---
+++ src/main/java/br/org/generation/projetointegrador/model/Categoria.java
@@ -0,0 +1,59 @@
+package br.org.generation.projetointegrador.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 50)
+	private String descricao;
+	
+	@NotNull
+	private boolean ativo;
+	
+	@Size(min = 5, max =  50)
+	private String palavraChave;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getPalavraChave() {
+		return palavraChave;
+	}
+
+	public void setPalavraChave(String palavraChave) {
+		this.palavraChave = palavraChave;
+	}
+
+}
